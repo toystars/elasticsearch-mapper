7fd0a197dc983d9038b9db35d651cd3bbd6cf362
Mustapha Babatunde <toystars2008@gmail.com>
2016-09-17 03:18:40 +0100

    Add basic clear, index and indexCount function

--- test/test.js
+++ test/test.js
@@ -4,9 +4,11 @@ var expect = require('chai').expect;
 var mapper = require('../index');
 
 
-describe('#ES-JS', function() {
-  it('should pass dummy test', function() {
-    var number = 10;
-    expect(number).to.equal(10);
+describe('#AddIndex', function() {
+  it('should return length of indices registered', function () {
+    // clear mapper to remove left-over config
+    mapper.clear();
+    mapper.index('Animals');
+    expect(mapper.indexCount()).to.equal(1);
   });
 });
