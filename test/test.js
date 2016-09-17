d9689d4b8cc5636d4701d37ab23111403d786f67
Mustapha Babatunde <toystars2008@gmail.com>
2016-09-17 09:54:53 +0100

    Add test for clear function

--- test/test.js
+++ test/test.js
@@ -4,7 +4,18 @@ var expect = require('chai').expect;
 var mapper = require('../index');
 
 
-describe('#AddIndex', function() {
+describe('#clear', function() {
+  it('should return 0 after clearing Mapper', function () {
+    mapper.index('Animals');
+    mapper.index('Plants');
+    expect(mapper.indexCount()).to.equal(2);
+    // clear mapper to remove left-over config
+    mapper.clear();
+    expect(mapper.indexCount()).to.equal(0);
+  });
+});
+
+describe('#index', function() {
   it('should return length of indices registered', function () {
     // clear mapper to remove left-over config
     mapper.clear();
