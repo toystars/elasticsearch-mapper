8e900fcc87868fa4627dea444b6f7b2dd0cb7eaf
Mustapha Babatunde <toystars2008@gmail.com>
2016-09-17 10:57:51 +0100

    Add default index configuration and tests for getIndex

--- test/test.js
+++ test/test.js
@@ -15,11 +15,56 @@ describe('#clear', function() {
   });
 });
 
+
 describe('#index', function() {
   it('should return length of indices registered', function () {
     // clear mapper to remove left-over config
     mapper.clear();
     mapper.index('Animals');
     expect(mapper.indexCount()).to.equal(1);
+  });
+});
+
+
+describe('#getIndex', function() {
+
+  it('should return the index object containing analyzers (index and search) and types mappings', function () {
+    // clear mapper to remove left-over config
+    mapper.clear();
+    mapper.index('Animals');
+    expect(mapper.getIndex('Animals')).to.deep.equal({
+      settings: {
+        'index.mapper.dynamic': false,
+        analysis: {
+          filter: {
+            edgeNGram_filter: {
+              type: 'edgeNGram',
+              min_gram: 3,
+              max_gram: 15,
+              token_chars: ['letter', 'digit', 'punctuation', 'symbol']
+            }
+          },
+          analyzer: {
+            edgeNGram_analyzer: {
+              type: 'custom',
+              tokenizer: 'standard',
+              filter: ['lowercase', 'asciifolding', 'edgeNGram_filter']
+            },
+            whitespace_analyzer: {
+              type: 'custom',
+              tokenizer: 'whitespace',
+              filter: ['lowercase', 'asciifolding']
+            }
+          }
+        }
+      },
+      mappings: {}
+    });
+  });
+
+  it('should return undefined if index is not registered', function () {
+    // clear mapper to remove left-over config
+    mapper.clear();
+    expect(mapper.getIndex('Persons')).to.be.undefined;
   });
 });
