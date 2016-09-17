2493b372ccabeb078ba3534a4988519e6e51ae9f
Mustapha Babatunde <toystars2008@gmail.com>
2016-09-17 02:44:53 +0100

    Setup npm project (test setup, coverage and badges for README)

---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * elasticsearch-mapper
+ * Copyright(c) 2016 Mustapha Babatunde Oluwaleke
+ * MIT Licensed
+ */
+
+'use strict';
+
+module.exports = require('./lib/elasticsearch-mapper');
---
+++ test/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var expect = require('chai').expect;
+var mapper = require('../index');
+
+
+describe('#ES-JS', function() {
+  it('should pass dummy test', function() {
+    var number = 10;
+    expect(number).to.equal(10);
+  });
+});
